89f5ed9d4a3f890da36020a58cdfd57ac03a8384
Jiang <zengzhi.jiang@capgemini.com>
2021-08-27 12:37:05 -0500

    Add css to sidebar

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,24 +2,32 @@ import React, { useState } from "react";
 import SidebarRow from "./SidebarRow";
 import "./Sidebar.css";
 
+import { BsHouseDoorFill } from "react-icons/bs";
+import { BsFillCollectionPlayFill } from "react-icons/bs";
+import { BsClockHistory } from "react-icons/bs";
+import { MdVideoLibrary } from "react-icons/md";
+import { MdExplore } from "react-icons/md";
+
 // import { connect } from 'react-redux';
 
 function Sidebar() {
 //   const [sidebarState, setSidebarState] = useState(false);
 //   const showSidebar = () => setSidebarState(!sidebarState);
 
+  const home = <BsHouseDoorFill />;
+  const explore = <MdExplore />
+  const sub = <BsFillCollectionPlayFill />;
+  const lib = <MdVideoLibrary />;
+  const history = <BsClockHistory />;
+  
   return (
-    <>
-      <div>
-        <h1>YouTube</h1>
+      <div className="bar">
+        <SidebarRow title="home" icon={home}/>
+        <SidebarRow title="explore" icon={explore}/>
+        <SidebarRow title="subscription" icon={sub}/>
+        <SidebarRow title="Library" icon={lib}/>
+        <SidebarRow title="History" icon={history}/>
       </div>
-
-      <div className="test">
-        <SidebarRow title="home" className="home" />
-        <SidebarRow title="subscription" className="home" />
-        <SidebarRow title="trending" className="home" />
-      </div>
-    </>
   );
 }
 
